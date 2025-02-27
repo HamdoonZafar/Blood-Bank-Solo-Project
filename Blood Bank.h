b001a6d607fdea02c45f0daef40420e1fcc0d17c
HamdoonZafar <hamdoonzafar29@gmail.com>
2025-02-27 12:29:01 +0500

    Add files via upload

---
+++ Blood Bank.h
@@ -0,0 +1,12 @@
+//
+// Created by Humdoon Zafar on 11/29/2024.
+//
+
+#ifndef BLOOD_BANK_H
+#define BLOOD_BANK_H
+void donateBlood();
+void receiveBlood();
+void loadFromFile();
+void adminLogin();
+void adminSignUp();
+#endif //BLOOD_BANK_H
